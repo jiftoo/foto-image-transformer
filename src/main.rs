2b7d0032cf878615dded0b34670a44043e8b8f86
Dan Kov <0x666c@mail.ru>
2022-01-15 15:19:56 +0300

    Some testing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     // proc::execute(std::env::args().skip(1).collect());
 
     let regex = Regex::new(r"([0-9]{1,3})x([0-9]{1,3})").unwrap();
-    for cap in regex.captures("640x480ssa").unwrap().iter().skip(1) {
+    for cap in regex.captures("640x480").unwrap().iter().skip(1) {
         println!("{:?}", cap.unwrap().as_str());
     }
 }
